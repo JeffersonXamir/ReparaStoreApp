a0a2893a0213f25a1ed5762d83e820609d95d833
Anchundia Jefferson <multisoft.janchundia@outlook.com>
2025-07-10 09:15:50 -0500

    MIGRACION - Se ajusta tabla clientes

--- ReparaStoreApp.Entities/Models/Cliente/Clientes.cs
+++ ReparaStoreApp.Entities/Models/Cliente/Clientes.cs
@@ -1,4 +1,5 @@
 ﻿using ReparaStoreApp.Entities.Models.Dispositivo;
+using ReparaStoreApp.Entities.Models.Security;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -11,13 +12,23 @@ namespace ReparaStoreApp.Entities.Models.Cliente
     public class Clientes
     {
         public int Id { get; set; }
-        [Required]
+        public string Codigo { get; set; }
         public string Nombre { get; set; }
+        public string PrimerNombre { get; set; }
+        public string SegundoNombre { get; set; }
+        public string PrimerApellido { get; set; }
+        public string SegundoApellido { get; set; }
+        public DateTime FechaNacimiento { get; set; }
         public string Correo { get; set; }
-        [Required]
         public string Telefono { get; set; }
         public string Direccion { get; set; }
+        public string Nota { get; set; }
         public DateTime FechaCreacion { get; set; }
+        public int UsuarioCreadorId { get; set; }
+        public User UsuarioCreador { get; set; }
+        public DateTime FechaEdicion { get; set; }
+        public int? UsuarioEdicionId { get; set; }
+        public User UsuarioEdicion { get; set; }
         public bool Activo { get; set; }
 
         public ICollection<Dispositivos> Dispositivos { get; set; }
