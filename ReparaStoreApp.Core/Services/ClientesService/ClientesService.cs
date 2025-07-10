796b9fe9cf5b1c780c6517ca41c0e07ac1393524
JeffersonXamir <46076279+JeffersonXamir@users.noreply.github.com>
2025-07-10 06:44:00 -0500

    Avances de clientes

---
+++ ReparaStoreApp.Core/Services/ClientesService/ClientesService.cs
@@ -0,0 +1,91 @@
+﻿using AutoMapper;
+using ReparaStoreApp.Common.Entities;
+using ReparaStoreApp.Core.Services.Login;
+using ReparaStoreApp.Data.Repositories.ClientesRepository;
+using ReparaStoreApp.Data.Repositories.Login;
+using ReparaStoreApp.Entities.Models.Cliente;
+using ReparaStoreApp.Entities.Models.Security;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReparaStoreApp.Core.Services.ClientesService
+{
+    public class ClientesService : IClientesService
+    {
+        private readonly IClientesRepository _ClientesRepository;
+        private readonly IMapper _mapper;
+
+        public ClientesService(IClientesRepository ClientesRepository, IMapper mapper)
+        {
+            _ClientesRepository = ClientesRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<Clientes> GetClientesByIdAsync(int id)
+        {
+            return await _ClientesRepository.GetByIdAsync(id);
+        }
+
+        public async Task<IEnumerable<Clientes>> SearchClientessAsync(string searchText, int page, int pageSize)
+        {
+            return await _ClientesRepository.SearchAsync(searchText, page, pageSize);
+        }
+
+        public async Task<int> GetClientesCountAsync(string searchText)
+        {
+            return await _ClientesRepository.GetCountAsync(searchText);
+        }
+
+        public async Task SaveClientesAsync(ClientesItem Clientes)
+        {
+            if (Clientes == null) return;
+            var ClientesDb = _mapper.Map<Clientes>(Clientes);
+            //ClientesDb.PasswordHash = BCrypt.Net.BCrypt.HashPassword(Clientes.PasswordHash);
+            //ClientesDb.CreatedAt = DateTime.UtcNow;
+            //ClientesDb.IsActive = true;
+            await _ClientesRepository.SaveAsync(ClientesDb);
+        }
+
+        public async Task UpdateClientesAsync(ClientesItem Clientes)
+        {
+            if (Clientes == null) return;
+            var ClientesDb = await _ClientesRepository.GetByIdAsync(Clientes.Id);
+
+            //ClientesDb.Code = Clientes.Code;
+            //ClientesDb.Name = Clientes.Name;
+            //ClientesDb.FirstName = Clientes.FirstName;
+            //ClientesDb.LastName = Clientes.LastName;
+            //ClientesDb.Email = Clientes.Email;
+            //ClientesDb.PhoneNumber = Clientes.PhoneNumber;
+            //ClientesDb.Note = Clientes.Note;
+
+
+            // Verificar si el campo de contraseña tiene algo
+            //if (!string.IsNullOrWhiteSpace(Clientes.PasswordHash))
+            //{
+            //    // Asumimos que se quiere cambiar la contraseña
+            //    ClientesDb.PasswordHash = BCrypt.Net.BCrypt.HashPassword(Clientes.PasswordHash);
+            //}
+
+            await _ClientesRepository.SaveAsync(ClientesDb);
+        }
+
+        public async Task ActivateClientesAsync(ClientesItem Clientes)
+        {
+            if (Clientes == null) return;
+            var ClientesDb = await _ClientesRepository.GetByIdAsync(Clientes.Id);
+            await _ClientesRepository.Activate(ClientesDb);
+        }
+
+        public async Task DeleteClientesAsync(ClientesItem Clientes)
+        {
+            if (Clientes == null) return;
+            var ClientesDb = await _ClientesRepository.GetByIdAsync(Clientes.Id);
+            await _ClientesRepository.Delete(ClientesDb);
+        }
+
+    }
+}
---
+++ ReparaStoreApp.Data/Repositories/ClientesRepository/ClientesRepository.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ReparaStoreApp.Entities.Models.Cliente;
+using ReparaStoreApp.Entities.Models.Security;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReparaStoreApp.Data.Repositories.ClientesRepository
+{
+    public  class ClientesRepository: IClientesRepository
+    {
+        private readonly AppDbContext _context;
+
+        public ClientesRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Clientes> GetByIdAsync(int id)
+        {
+            return await _context.Clientes
+                .FirstOrDefaultAsync(u => u.Id == id);
+        }
+
+        public async Task<IEnumerable<Clientes>> SearchAsync(string searchText, int page, int pageSize)
+        {
+            return await _context.Clientes
+                .Where(u => string.IsNullOrEmpty(searchText) ||
+                           u.Nombre.Contains(searchText))
+                //u.FullName.Contains(searchText))
+                .OrderBy(u => u.Nombre)
+                .Skip((page - 1) * pageSize)
+                .Take(pageSize)
+                .ToListAsync();
+        }
+        
+        public async Task<int> GetCountAsync(string searchText)
+        {
+            return await _context.Clientes
+                .Where(u => string.IsNullOrEmpty(searchText) ||
+                          //u.c.Contains(searchText) ||
+                          u.Nombre.Contains(searchText))
+                //u..Contains(searchText))
+                .CountAsync();
+        }
+
+        public async Task SaveAsync(Clientes Clientes)
+        {
+            if (Clientes.Id == 0)
+            {
+                _context.Clientes.Add(Clientes);
+            }
+            else
+            {
+                _context.Clientes.Update(Clientes);
+            }
+
+            _context.SaveChanges();
+        }
+
+        public async Task Delete(Clientes Clientes)
+        {
+            Clientes.Activo = false;
+            _context.SaveChanges();
+        }
+
+        public async Task Activate(Clientes Clientes)
+        {
+            Clientes.Activo = true;
+            _context.SaveChanges();
+        }
+    }
+}
