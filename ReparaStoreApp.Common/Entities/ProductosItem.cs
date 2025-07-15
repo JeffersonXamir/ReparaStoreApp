d6d74cc0355f33dde1c72f347e47885c5e3efab9
JeffersonXamir <46076279+JeffersonXamir@users.noreply.github.com>
2025-07-14 20:09:58 -0500

    Jefferson develop (#2)
    * Avance MainView
    avances de menu principal
    
    * creacion de menu dinamico y cargar opcion del menu
    
    * creacion de cinta de opciones para el manejo de acciones.
    
    * validaciones de la cinta de opciones
    
    * se añade control base para la busquedas en mantenimientos
    
    * se cambia inicio de control a login
    
    * Se añade instalador de la aplicacion mas creacion de la base de datos
    
    * Se añade icono en el instalador
    
    * Se añade icono en el instalador
    
    * se omite carga de instalador
    
    * se añade mejoras al instalador
    
    * Cambios para hacer migraciones en interno
    
    * omitir exe
    
    * MIGRACION - se edita la tabla de usuarios
    
    * avances en logica de actualizar usuario
    
    * mantenimiento de usuario
    se arreglan estilos de navegacion
    progres bar en inicio
    
    * comentarios
    
    * MIGRACION - Se crean nuevas definiciones de tablas
    
    * Avances de clientes
    
    * MIGRACION - Se ajusta tabla clientes
    
    * CLIENTES - avances de mantenimiento
    
    * CLIENTES - CRUD avances
    
    * MIGRACION - se modifica tabla de dispositivos
    
    * se finaliza Clientes
    
    * se finaliza dispositivos
    se finaliza cierre de session
    se edita fondo del Home
    
    * Mejoras en iconos y navegacion
    se mejora diseño de generic List
    
    * MIGRACION - Restructuracion de tablas
    
    * avances de productos y servicios backend
    
    * se finaliza opcion de servicios y productos
    
    * Mejora en iconos y Home
    
    * Cambio de icono fondo blanco
    
    * Avances documento - reparacion
    
    * avances front end Reparaciones document
    se crea dialogos genericos
    
    * Logica en datagrid y eventos
    
    * Creacion de documento de reparaciones
    
    * Creacion de Dialog generico para documentos y guardado de reparaciones
    
    * no se cargaban roles en usuarios new
    
    * Git nuevos cambios cambios botones
    
    * se integran reportes de inventario
    
    * MIGRACION - carga inicial de registros
    
    * Se finaliza desarrollo de Repara Store App con alcances, pendiente crear la nota de venta
    
    * Validacion extra en docuemnto de reparaciones
    
    * Mejorar en el guardado de productos y servicios
    
    ---------
    
    Co-authored-by: Anchundia Jefferson <multisoft.janchundia@outlook.com>

--- ReparaStoreApp.Common/Entities/ProductosItem.cs
+++ ReparaStoreApp.Common/Entities/ProductosItem.cs
@@ -10,18 +10,48 @@ namespace ReparaStoreApp.Common.Entities
 {
     public  class ProductosItem : Item
     {
-        public string Descripcion { get; set; } = "";
-        public string Nota { get; set; } = "";
-        public decimal Precio { get; set; }
-        public TipoItem Tipo { get; set; } // Producto o Servicio
-        
+        private string _descripcion = "";
+        public string Descripcion
+        {
+            get { return _descripcion; }
+            set { _descripcion = value; NotifyOfPropertyChange(() => Descripcion); }
+        }
+
+        private string _nota = "";
+        public string Nota
+        {
+            get { return _nota; }
+            set { _nota = value; NotifyOfPropertyChange(() => Nota); }
+        }
+
+        private decimal _precio;
+        public decimal Precio
+        {
+            get { return _precio; }
+            set { _precio = value; NotifyOfPropertyChange(() => Precio); }
+        }
+
+        private TipoItem _tipo;
+        public TipoItem Tipo
+        {
+            get { return _tipo; }
+            set { _tipo = value; NotifyOfPropertyChange(() => Tipo); }
+        }
+
         private bool _activo;
         public bool Activo
         {
             get { return _activo; }
             set { _activo = value; NotifyOfPropertyChange(() => Activo); }
         }
-        public bool TieneIVA { get; set; } = true;
+
+        private bool _tieneIVA = true;
+        public bool TieneIVA
+        {
+            get { return _tieneIVA; }
+            set { _tieneIVA = value; NotifyOfPropertyChange(() => TieneIVA); }
+        }
+        
         public DateTime FechaCreacion { get; set; }
         public int UsuarioCreadorId { get; set; }
         public User UsuarioCreador { get; set; }
